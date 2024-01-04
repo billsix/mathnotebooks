d1af097b073cfcff9417404b04fb9bb821c6af60
William Emerison Six <billsix@gmail.com>
2024-01-03 19:51:40 -0500

    Moved to one notebook

--- prealgebra/utils.py
+++ prealgebra/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 William Emerison Six
+# Copyright (c) 2023-2024 William Emerison Six
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,6 +24,7 @@
 # -------------
 # %%
 import sympy
+import itertools
 from galgebra.printer import latex
 
 # tell sympy to use our printing by default
@@ -98,5 +99,114 @@ def divide_mixed_fractions(first, second):
             + "}{"
             + str(first[2] * (second[0] * second[2] + second[1]))
             + "}}"
+        )
+    )
+
+
+def add_decimals(first, second):
+    display(Markdown("**Align the numbers on the decimal**"))
+
+    def _split_decimals(first, second):
+        first_whole, first_dec = first.split(".")
+        second_whole, second_dec = second.split(".")
+
+        return (first_whole, first_dec), (second_whole, second_dec)
+
+    (first_whole, first_dec), (second_whole, second_dec) = _split_decimals(first, second)
+
+    display(
+        Math(
+            r"""\begin{align}
+"""
+            + first_whole
+            + r"""&."""
+            + first_dec
+            + r"""\\
+ +\quad """
+            + second_whole
+            + r"&."
+            + second_dec
+            + r"""\\
+\hline
+\end{align}"""
+        )
+    )
+
+    display(Markdown("**Put zeros for placeholders**"))
+
+    def _split_decimals(first, second):
+        first_whole, first_dec = first.split(".")
+        second_whole, second_dec = second.split(".")
+
+        def _put_zeros_on_end(first_dec, second_dec):
+            max_len = max(len(first_dec), len(second_dec))
+            return (
+                first_dec + "".join(list(itertools.repeat("0", max_len - len(first_dec)))),
+                second_dec + "".join(list(itertools.repeat("0", max_len - len(second_dec)))),
+            )
+
+        first_dec_zero_padded, second_dec_zero_padded = _put_zeros_on_end(first_dec, second_dec)
+
+        return (first_whole, first_dec_zero_padded), (second_whole, second_dec_zero_padded)
+
+    (first_whole, first_dec), (second_whole, second_dec) = _split_decimals(first, second)
+
+    display(
+        Math(
+            r"""\begin{align}
+"""
+            + first_whole
+            + r"""&."""
+            + first_dec
+            + r"""\\
+ +\quad """
+            + second_whole
+            + r"&."
+            + second_dec
+            + r"""\\
+\hline
+\end{align}"""
+        )
+    )
+
+    def _actually_add_them(first_whole, first_dec, second_whole, second_dec):
+        the_len = max(len(first_dec), len(second_dec))
+        dec_added = str(int(first_dec) + int(second_dec))
+
+        if len(dec_added) > the_len:
+            extra_int = dec_added[:1]
+            dec_to_return = dec_added[1:]
+        else:
+            extra_int = "0"
+            dec_to_return = dec_added
+
+        whole_added = str(int(first_whole) + int(second_whole) + int(extra_int))
+
+        return whole_added, dec_to_return
+
+    result_whole, result_dec = _actually_add_them(first_whole, first_dec, second_whole, second_dec)
+
+    display(Markdown("**Add like for integers**"))
+
+    display(
+        Math(
+            r"""\begin{align}
+"""
+            + first_whole
+            + r"""&."""
+            + first_dec
+            + r"""\\
+ +\quad """
+            + second_whole
+            + r"&."
+            + second_dec
+            + r"""\\
+\hline
+"""
+            + result_whole
+            + r""" &."""
+            + result_dec
+            + r"""
+\end{align}"""
         )
     )
